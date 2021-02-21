cdbe6721fa0dc5d3e7c02ed9429d8aadeeebcd91
Renato Athaydes <renato@athaydes.com>
2021-02-21 11:51:06 +0100

    Updated UI example to use KanvasObject.

--- demo/groovy/src/ui.groovy
+++ demo/groovy/src/ui.groovy
@@ -1,11 +1,11 @@
 import com.athaydes.kanvas.Kanvas
+import com.athaydes.kanvas.gr.KanvasObject
 import com.athaydes.kanvas.gr.KanvasScript
+import groovy.beans.Bindable
 import groovy.transform.BaseScript
-import groovy.transform.Canonical
 import groovy.transform.CompileStatic
 import javafx.geometry.BoundingBox
 import javafx.scene.paint.Color
-import javafx.scene.paint.Paint
 import javafx.scene.text.Font
 
 @BaseScript KanvasScript baseScript
@@ -15,57 +15,77 @@ width 340
 height 130
 
 background Color.WHITE
-fill Color.RED
-stroke Color.BLACK
 
-button = new Button(kanvas: kanvas, x: 20, y: 40, w: 150, h: 50, text: 'Click Me!')
-mouse.onClick { button.onClick() }
+// shared state
+isActivated = new ActivatedState(on: false)
 
-loop { long dt ->
-    clear()
-    button.draw(dt)
-    if (button.isOn) {
-        fill Color.RED
-        at 200, 20 circle(50, true)
-    } else {
-        at 200, 20 circle(50)
-    }
+// UI components
+button = new Button(x: 20, y: 40, w: 150, h: 50, activatedState: isActivated, text: 'Click Me!')
+circle = new Circle(x: 200, y: 20, radius: 50, activatedState: isActivated)
+
+kanvasObjects(button, circle)
+
+@CompileStatic
+@Bindable
+class ActivatedState {
+    boolean on
 }
 
-@Canonical
 @CompileStatic
-class Button {
+@Bindable
+class Circle implements KanvasObject {
+    double x, y, radius
+    ActivatedState activatedState
+
+    @Override
+    void draw(Kanvas kanvas) {
+        kanvas.fill activatedState.on ? Color.RED : Color.WHITE
+        kanvas.at x, y circle(radius, true) stroke Color.BLACK circle(radius)
+    }
+
+}
+
+@CompileStatic
+@Bindable
+class Button implements KanvasObject {
     double x, y, w, h
-    Paint color = Color.LIGHTGRAY
-    Paint hoverColor = Color.INDIGO
-    Paint clickColor = Color.PURPLE
-    Paint textColor = Color.BLACK
-    Paint hoverTextColor = Color.WHITE
-    String text = 'Button'
-    Kanvas kanvas
-    boolean isOn
-    private long justClickedTime
+    boolean isMouseOn
+    String text
+    long justClickedTime
+    ActivatedState activatedState
+    BoundingBox bounds
 
-    void onClick() {
-        if (kanvas.mouse.isOn(boundingBox)) {
-            isOn = !isOn
-            justClickedTime = 120
+    @Override
+    void init(Kanvas kanvas) {
+        bounds = kanvas.bounds(x, y, w, h)
+        kanvas.mouse.onClick {
+            if (isMouseOn) {
+                activatedState.on = !activatedState.on
+                setJustClickedTime 120
+            }
+        }
+        kanvas.mouse.onMove {
+            def isOn = kanvas.mouse.isOn(bounds)
+            if (isMouseOn != isOn) {
+                setIsMouseOn isOn
+            }
         }
     }
 
-    void draw(long dt) {
-        def isHover = kanvas.mouse.isOn(boundingBox)
+    @Override
+    void update(Kanvas kanvas, long dt) {
         if (justClickedTime > 0) {
-            justClickedTime -= dt
+            setJustClickedTime justClickedTime - dt
         }
-        kanvas.fill(justClickedTime > 0 ? clickColor : isHover ? hoverColor : color)
-        kanvas.at x, y rectangle(w, h, true)
-        kanvas.font(Font.font(24), isHover ? hoverTextColor : textColor)
-        kanvas.at x + 28, y + 32 text(text)
     }
 
-    BoundingBox getBoundingBox() {
-        kanvas.bounds(x, y, w, h)
+    @Override
+    void draw(Kanvas kanvas) {
+        kanvas.fill(justClickedTime > 0 ? Color.PURPLE :
+                isMouseOn ? Color.INDIGO : Color.LIGHTGRAY)
+        kanvas.at x, y rectangle(w, h, true)
+        kanvas.font(Font.font(24), isMouseOn ? Color.WHITE : Color.BLACK)
+        kanvas.at x + 28, y + 32 text(text)
     }
 
 }
